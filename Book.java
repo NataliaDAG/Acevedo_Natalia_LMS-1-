e0df53fdb7c485c1aa0b27a3abbbfdcffdd48027
NataliaDAG <143017619+NataliaDAG@users.noreply.github.com>
2023-10-08 11:30:00 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,6 +1,7 @@
 // Natalia Acevedo
+// Natalia Acevedo
 // CEN-3024C
-// last updated 9/6/2023
+// last updated 10/8/2023
 // Book
 // The function of this class is to define what a book is based on 3 key factors.--
 //-- The factors are the ID, Title, and Author of the book.
@@ -8,7 +9,8 @@ public class Book {
 	private int id;
     private String title;
     private String author;
-    //Here it Defines what are the ID's, Title's, and author's of the books are.
+    private int barcode;
+    //Here it Defines what are the ID's, Title's, barcode, and author's of the books are.
     public Book(int id, String title, String author) {
         this.id = id;
         this.title = title;
@@ -24,5 +26,8 @@ public class Book {
 
     public String getAuthor() {
         return author;
+    }
+    public int getBarcode() {
+        return barcode;
     }
 }
