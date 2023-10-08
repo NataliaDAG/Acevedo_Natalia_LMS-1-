2f099852bfc13f01c541d9445c3ddbd384730c1a
NataliaDAG <143017619+NataliaDAG@users.noreply.github.com>
2023-10-08 11:30:39 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,6 +1,6 @@
 // Natalia Acevedo
 // CEN-3024C
-// last updated 9/6/2023
+// last updated 10/8/2023
 // library
 // The function of this class is to provide details to the main class on how the list should look like.--
 //-- Also it provide information on how to remove a book from the list to the main class.
@@ -38,10 +38,65 @@ public class Library {
         System.out.println("Book with ID " + id + " is already checked out.");
     }
 }
+    public void removeBookByBarcode(int barcode) { //Here the library will tell the main LMS how to remove a book with a barcode from the library
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getBarcode() == barcode) {
+                iterator.remove();
+                System.out.println("Book with barcode " + barcode + " has been removed from the library.");
+                return;
+            }
+        }
+        System.out.println("Book with barcode " + barcode + " was not found in the library.");
+    }
+
+    public void removeBookByTitle(String title) { //Here the library will tell the main LMS how to remove a book by title alone and confirm it
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                iterator.remove();
+                System.out.println("Book with title \"" + title + "\" has been removed from the library.");
+                return;
+            }
+        }
+        System.out.println("Book with title \"" + title + "\" was not found in the library.");
+    }
+
+    public void checkOutBookByTitle(String title) { //Here the library will tell the main LMS how to check out a book by title alone and confirm it
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                if (!checkedOutBooks.contains(book.getId())) {
+                    checkedOutBooks.add(book.getId());
+                    System.out.println("Book with title \"" + title + "\" has been checked out.");
+                } else {
+                    System.out.println("Book with title \"" + title + "\" is already checked out.");
+                }
+                return;
+            }
+        }
+        System.out.println("Book with title \"" + title + "\" was not found in the library.");
+    }
+
+    public void checkInBookByTitle(String title) {
+        for (Book book : books) { // here it checks if the book has been checked in or checked out
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                if (checkedOutBooks.contains(book.getId())) {
+                    checkedOutBooks.remove((Integer) book.getId());
+                    System.out.println("Book with title \"" + title + "\" has been checked in.");
+                } else {
+                    System.out.println("Book with title \"" + title + "\" is not checked out.");
+                }
+                return;
+            }
+        }
+        System.out.println("Book with title \"" + title + "\" was not found in the library.");
+    }
     // Here it prints out the format of how the list view should look like for all the books.
     public void displayBooks() {
         for (Book book : books) {
-        	System.out.println("ID: " + book.getId() + ", Title: " + book.getTitle() + ", Author: " + book.getAuthor());
+        	System.out.println("ID: " + book.getId() + ", Title: " + book.getTitle() + ", Author: " + book.getAuthor() + ", Barcode: " + book.getBarcode());
         }
     }
 }
