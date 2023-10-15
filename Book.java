a1ab1e7f3a1b208c1234e70e8e6b6e1c4ce51891
NataliaDAG <143017619+NataliaDAG@users.noreply.github.com>
2023-10-15 14:37:49 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,20 +1,23 @@
 // Natalia Acevedo
-// Natalia Acevedo
 // CEN-3024C
 // last updated 10/8/2023
 // Book
 // The function of this class is to define what a book is based on 3 key factors.--
 //-- The factors are the ID, Title, and Author of the book.
+import java.util.Date;
 public class Book {
-	private int id;
-    private String title;
-    private String author;
-    private int barcode;
+	 private int id;
+	 private String title;
+	 private String author;
+	 private int barcode;
+	 private Date dueDate;
     //Here it Defines what are the ID's, Title's, barcode, and author's of the books are.
-    public Book(int id, String title, String author) {
+    public Book(int id, String title, String author, int barcode) {
         this.id = id;
         this.title = title;
         this.author = author;
+        this.barcode = barcode;
+        this.dueDate = null;
     }
     public int getId() {
         return id;
@@ -29,5 +32,12 @@ public class Book {
     }
     public int getBarcode() {
         return barcode;
+    }
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
     }
 }
